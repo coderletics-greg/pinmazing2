78cfa97c69cd70b8324a06e0a48691394f2c0343
coderletics-greg <greg.aubert@yahoo.co.uk>
2014-11-29 00:31:17 +0000

    asdf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinmazing::Application.config.secret_key_base = 'c399b81a019ed3ae3a9bc70b080992cc6be80907f84ecfa2a7e253863cb37dd72e85a0c1c5624caefa3b00ca459aa1d61d9265929e8844c25952621cf490e37a'
